28452acfcedd3c611f6335ebfda4b1c7fe4c8d55
Norbert Augusztin <anorbee95@gmail.com>
2024-07-01 16:15:23 +0200

    revert to require

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,15 +1,15 @@
-import nodemailer from "nodemailer";
+const nodemailer = require('nodemailer');
 
-export default async function handler(event) {
-  console.log("Function triggered");
-  console.log("Event body:", event.body);
+exports.handler = async (event) => {
+  console.log('Function triggered');
+  console.log('Event body:', event.body);
 
   try {
     const { to, subject, text } = JSON.parse(event.body);
-    console.log("Parsed email data:", { to, subject, text });
+    console.log('Parsed email data:', { to, subject, text });
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      service: 'gmail',
       auth: {
         user: process.env.VITE_EMAIL,
         pass: process.env.VITE_PASSWORD,
@@ -17,26 +17,26 @@ export default async function handler(event) {
     });
 
     const mailOptions = {
-      from: process.env.EMAIL,
+      from: process.env.VITE_EMAIL,
       to,
       subject,
       text,
     };
 
-    console.log("Sending email with options:", mailOptions);
+    console.log('Sending email with options:', mailOptions);
     await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully");
+    console.log('Email sent successfully');
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Email sent successfully" }),
+      body: JSON.stringify({ message: 'Email sent successfully' }),
     };
   } catch (error) {
-    console.error("Error sending email:", error);
+    console.error('Error sending email:', error);
 
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.toString() }),
     };
   }
-}
+};
