aa90c49fdb237c60799ba9b0f316940307aa3970
Augusztin Norbert <anorbee95@gmail.com>
2024-07-03 15:09:38 +0200

    update gallery with arrows

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import miniLeaves from "../assets/miniLeaves.png";
 import fekvo1 from "../assets/fekvo/fekvo1.jpg";
 import fekvo2 from "../assets/fekvo/fekvo2.jpg";
@@ -10,7 +11,17 @@ import fekvo7 from "../assets/fekvo/fekvo7.jpg";
 import fekvo8 from "../assets/fekvo/fekvo8.jpg";
 import fekvo9 from "../assets/fekvo/fekvo9.jpg";
 
-const images = [fekvo1, fekvo2, fekvo3, fekvo4, fekvo5, fekvo6, fekvo7, fekvo8, fekvo9];
+const images = [
+  fekvo1,
+  fekvo2,
+  fekvo3,
+  fekvo4,
+  fekvo5,
+  fekvo6,
+  fekvo7,
+  fekvo8,
+  fekvo9,
+];
 images.sort(() => Math.random() - 0.5);
 
 export default function Gallery() {
@@ -20,43 +31,48 @@ export default function Gallery() {
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (!isModalOpen) return;
-      if (e.key === 'ArrowRight') nextImage();
-      if (e.key === 'ArrowLeft') prevImage();
-      if (e.key === 'Escape') closeModal();
+      if (e.key === "ArrowRight") nextImage();
+      if (e.key === "ArrowLeft") prevImage();
+      if (e.key === "Escape") closeModal();
     };
 
-    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener("keydown", handleKeyDown);
     };
   }, [isModalOpen, currentImage]);
 
   const openModal = (idx) => {
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
     setCurrentImage(idx);
     setModalOpen(true);
   };
 
   const closeModal = () => {
-    document.body.style.overflow = 'auto';
+    document.body.style.overflow = "auto";
     setModalOpen(false);
   };
 
   const nextImage = () => setCurrentImage((currentImage + 1) % images.length);
-  const prevImage = () => setCurrentImage((currentImage - 1 + images.length) % images.length);
+  const prevImage = () =>
+    setCurrentImage((currentImage - 1 + images.length) % images.length);
 
   const handleClickOutside = (event) => {
-    if (event.target.classList.contains('modal-backdrop')) {
+    if (event.target.classList.contains("modal-backdrop")) {
       closeModal();
     }
   };
 
   return (
-    <section id='gallery' className="bg-white">
+    <section id="gallery" className="bg-white">
       <div className="pt-20 pb-10 lg:pb-20 flex flex-col gap-4 items-center justify-center">
         <h1 className="relative uppercase font-gilda text-4xl m-4 text-custom-pink">
-          <img className="absolute -top-8 left-16 h-12" src={miniLeaves} alt="miniLeaves" />
+          <img
+            className="absolute -top-8 left-16 h-12"
+            src={miniLeaves}
+            alt="miniLeaves"
+          />
           Galéria
         </h1>
         <div className="max-w-screen flex justify-center flex-wrap 2xl:grid 2xl:grid-cols-3 gap-8 my-4 mx-auto">
@@ -77,9 +93,37 @@ export default function Gallery() {
       </div>
 
       {isModalOpen && (
-        <div className="z-50 modal-backdrop fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center" onClick={handleClickOutside}>
+        <div
+          className="z-50 modal-backdrop fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center"
+          onClick={handleClickOutside}
+        >
           <div className="modal-content relative p-4 rounded-lg md:max-w-[60%] mx-auto">
-            <img className="w-full h-auto rounded-md" src={images[currentImage]} alt={`image ${currentImage}`} />
+            <div
+              onClick={prevImage}
+              className="cursor-pointer select-none absolute top-1/2 md:-mt-6 left-5 md:-left-12 text-2xl md:text-6xl text-zinc-400"
+            >
+              <FaAngleLeft />
+            </div>
+            <div
+              onClick={prevImage}
+              className="cursor-pointer absolute left-0 h-[96%] w-1/2 bg-opacity-100 z-1000"
+            ></div>
+            <img
+              className="w-full h-auto rounded-md"
+              src={images[currentImage]}
+              alt={`image ${currentImage}`}
+            />
+            <div
+              onClick={nextImage}
+              className="cursor-pointer absolute right-0 top-4 h-[96%] w-1/2 bg-opacity-100 z-1000"
+            ></div>
+
+            <div
+              onClick={nextImage}
+              className="cursor-pointer select-none absolute top-1/2 md:-mt-6 right-5 md:-right-12 text-2xl md:text-6xl text-zinc-400"
+            >
+              <FaAngleRight />
+            </div>
           </div>
         </div>
       )}
